d389201af8e45fdd9201b20b4dfe9c194cf4a04f
nseon <nseon@student.42lyon.fr>
2025-09-09 10:24:06 +0200

    push

--- src/render/ss_utils.c
+++ src/render/ss_utils.c
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 13:16:30 by nseon             #+#    #+#             */
-/*   Updated: 2025/09/09 09:29:56 by nseon            ###   ########.fr       */
+/*   Updated: 2025/09/09 10:20:35 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ float frandom(uint8_t const random[RAY_NBR], int min, int max)
 		i -= RAY_NBR;
 	nb = (float)(random[i]) / ((float)256 / (max - min));
 	nb += min;
-	i += 1;
+	i += 3;
 	return (nb);
 }
 
