a1aef69ea5651a0980da4ed1f569fdc7267a4d9b
nseon <nseon@student.42lyon.fr>
2025-09-10 11:03:46 +0200

    test on indirect light

--- src/maths/lighting/lighting.c
+++ src/maths/lighting/lighting.c
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 15:24:02 by pjarnac           #+#    #+#             */
-/*   Updated: 2025/09/09 17:20:38 by nseon            ###   ########.fr       */
+/*   Updated: 2025/09/10 10:38:29 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,13 @@ t_fcolor	indirect_light(t_world *w, t_pre_compute *pc, int n)
 	distance = i->t;
 	w->xs.count -= xs.count;
 	rcolor = color_at(w, r, -1);
+	// color_add(rcolor, color_at(w, r, 1));
 	if (distance < 1)
 		distance = 1;
-	rcolor = cap_color(rcolor);
+	// rcolor = cap_color(rcolor);
 	// if (rcolor.r > 1 && rcolor.g > 1 && rcolor.b > 1)
 	// 	printf("r: %f\ng: %f\nb: %f\n\n", rcolor.r, rcolor.g, rcolor.b);
-	return (col_scalar(color_mul(rcolor, pc->obj->mat.col), 1.0 / (distance * distance)));
+	return (color_mul(rcolor, pc->obj->mat.col));
 }
 
 t_fcolor	blend_additives(t_world *w, t_fcolor col, t_pre_compute *pc, int n)
