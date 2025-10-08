02f1e125f507de2fa66b525b9af04508749df0c0
Pierre Jarnac <pjarnac@student.42lyon.fr>
2025-10-08 11:41:13 +0200

    finito

--- src/hooks/keys.c
+++ src/hooks/keys.c
@@ -21,4 +21,6 @@ void	gui_keys(int keycode, void *p)
 
 	if (keycode == XK_quoteleft)
 		get_by_id(&ctx->win, RENDER_CTN_ID)->hide ^= true;
+	else if (keycode == XK_Escape)
+		end_loop(&ctx->win);
 }
